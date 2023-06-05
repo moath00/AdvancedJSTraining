eeb77b148fcf290fee41db2df8edc8a3cc27eee7
moath00 <moath.n.h20@gmail.com>
2023-06-05 23:31:49 +0300

    final result

--- finalProject/moviCatalog.js
+++ finalProject/moviCatalog.js
@@ -7,7 +7,8 @@ let catalogArray = [];
 
 const readJson = (link) => {
   try {
-    const dataFromJson = JSON.parse(fs.readFileSync(link, "utf-8"));
+    const rowDataJson = fs.readFileSync(link, "utf-8");
+    const dataFromJson = JSON.parse(rowDataJson);
     return dataFromJson;
   } catch (err) {
     console.log("Error happened while reading");
@@ -26,17 +27,22 @@ const writeJson = (data) => {
   }
 };
 
-catalogArray = readJson("./catalog.json");
 
 const getMovies = async () => {
     try {
         const url =
-          "https://api.themoviedb.org/3/keyword/Eng.Moath/movies?include_adult=false&language=en-US&page=1";
-        const response = await fetch(url, { method : "GET" });
-        console.log(response, "this is the response");
+          "https://api.themoviedb.org/3/trending/movie/week?language=en-US";
+        const options = {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MzFmN2M2NDg1YzRjMDk0ZTYzMTBkYzAwY2RiZmVkMiIsInN1YiI6IjY0N2Q5OWQ0Y2Y0YjhiMDEyMjc3MDQwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DVN7wbPHMy8FuCEDZeH2JhaElN4fjrtQgXDOGKXXvqM",
+          },
+        };
+        const response = await fetch(url, options);
         const data = await response.json();
-        console.log(data, "this is the data");
-        return data;
+        return data.results;
     }
     catch (err) {
         console.log("Failed to get movies from API.");
@@ -44,32 +50,37 @@ const getMovies = async () => {
     }
 };
 
-const addAPIMoviesToCatalog = (data) => {
-    let movies = JSON.parse(data);
-    for (const movie of movies) {
-        const newMovie = new Movie((catalogArray.length), movie.title, movie.Release, movie.Genre.Name , movie.Director.name, movie.rating );
-        catalogArray.push(newMovie);
-        writeJson(catalogArray);
-    }
+
+const addAPIMoviesToCatalog = (movies) => {
+  for (let movie of movies) {
+    const newMovie = new Movie(
+      movie.id,
+      movie.title,
+      movie.overview,
+      `${movie.release_date[0]}${movie.release_date[1]}${movie.release_date[2]}${movie.release_date[3]}`,
+      movie.Type,
+      movie.vote_average + "/10"
+    );
+    catalogArray.push(newMovie);
+  }
+  return catalogArray;
 };
 
-getMovies();
-// addAPIMoviesToCatalog(getMovies());
+// getMovies();
 
 const userAddMovie = () => {
-  let movieId =
-    catalogArray.length > 0 ? catalogArray[catalogArray.length - 1].Id + 1 : 0;
+  let movieId = `user${catalogArray.length}`;
   let title = input("Enter title: ");
-  let releaseYear = input("Enter year: ");
-  let genre = input("Enter genre: ");
   let description = input("Enter description: ");
+  let year = input("Enter year: ");
+  let genre = input("Enter genre: ");
   let rate = input("Enter rate: ");
   const movie = new Movie(
     movieId,
     title,
-    releaseYear,
-    genre,
     description,
+    year,
+    genre,
     rate
   );
   catalogArray.push(movie);
@@ -79,54 +90,63 @@ const userAddMovie = () => {
 const updateMovie = () => {
   let movieId = input("Enter the movie ID to update: ");
   for (let i = 0; i < catalogArray.length; i++) {
-    if (catalogArray[i].Id === movieId) {
-      let newTitle = input("Enter the new title: ");
-      let newReleaseYear = input("Enter the new release year: ");
-      let newGenre = input("Enter the new genre: ");
-      let newDescription = input("Enter the new description: ");
-      let newRate = input("Enter the new rate: ");
-      catalogArray[i] = {
-        ...catalogArray[i], // Keep existing properties
-        Title: newTitle,
-        Year: newReleaseYear,
-        Genre: newGenre,
-        Description: newDescription,
-        Rate: newRate,
-      };
-      break;
+    if (catalogArray[i].id == movieId) {
+        let newTitle = input("Enter the new title: ");
+        let newYear = input("Enter the new release year: ");
+        let newGenre = input("Enter the new genre: ");
+        let newDescription = input("Enter the new description: ");
+        let newRate = input("Enter the new rate: ");
+        catalogArray[i] = {
+            ...catalogArray[i],
+            title: newTitle,
+            description: newDescription,
+            year: newYear,
+            genre: newGenre,
+            rating: newRate
+        };
+        break;
     }
   }
   writeJson(catalogArray);
 };
 
 const printCatalog = () => {
-    data = readJson("./test.json");
+    let data = readJson("./test.json");
     data.forEach((movie) => {
     console.log(`===========================================
-Movie Id: ${movie.Id}
-Title: ${movie.Title}
-Released in: ${movie.Year}
-Directed by: ${movie.Director}
-Description: ${movie.Description}
-===========================================`);
+Movie Id: ${movie.id}
+Title: ${movie.title}
+Description: ${movie.description}
+Released in: ${movie.year}
+Genre: ${movie.genre}
+Rating: ${movie.rating}
+-------------------------------------------`);
   });
 };
 
 const deleteMovie = () => {
   let movieId = input("Enter the movie ID to delete: ");
-  catalogArray = catalogArray.filter((movie) => movie.Id !== movieId);
+  catalogArray = catalogArray.filter((movie) => {
+        if (movie.id == movieId)
+            return movie = "";
+        else
+            return movie;
+});
   writeJson(catalogArray);
 };
 
 const searchAboutMovie = () => {
   let movieTitle = input("Enter the movie title to search about: ");
   for (let i = 0; i < catalogArray.length; i++) {
-    if ((catalogArray[i].Title).includes(movieTitle)) {
-      console.log(`Movie Id: ${catalogArray[i].Id}
-Title: ${catalogArray[i].Title}
-Released in: ${catalogArray[i].Year}
-Directed by: ${catalogArray[i].Director}
-Description: ${catalogArray[i].Description}`);
+    if (((catalogArray[i].title).toLowerCase()).includes(movieTitle.toLocaleLowerCase())) {
+      console.log(`===========================================
+Movie Id: ${catalogArray[i].id}
+Title: ${catalogArray[i].title}
+Description: ${catalogArray[i].description}
+Released in: ${catalogArray[i].year}
+Genre: ${catalogArray[i].genre}
+Rating: ${catalogArray[i].rating}
+-------------------------------------------`);
       break;
     }
   }
@@ -134,27 +154,36 @@ Description: ${catalogArray[i].Description}`);
 
 const filterMovies = () => {
   let filterBy = input(
-    "Enter the criteria to filter by (year, genre, or director): "
-  );
+    "Enter the criteria to filter by (year, genre, or rate): "
+  ).toLocaleLowerCase;
   let filterValue = input("Enter the filter value: ");
-  let filteredData = catalogArray.filter(
-    (movie) => movie[filterBy] === filterValue
-  );
-  console.log(filteredData);
+  catalogArray.filter((movie) => {
+      if (movie[filterBy].includes(filterValue)) {
+          console.log(`===========================================
+Movie Id: ${movie.id}
+Title: ${movie.title}
+Description: ${movie.description}
+Released in: ${movie.year}
+Genre: ${movie.genre}
+Rating: ${movie.rating}
+-------------------------------------------`);
+      }
+    });
 };
 
-const main = () => {
-  catalogArray = readJson();
-  while (true) {
-    console.log(`===========================================
+const main = async () => {
+    while (true) {
+        catalogArray = readJson("./test.json");
+        console.log(`===========================================
 ----------- MOVIES CATALOG INFO -----------
 ===========================================
 1) ADD new movie
 2) Update movie
 3) Display movies catalog
-4) Delete movie
-5) Search about movie
-6) Filter movies
+4) Add API movies
+5) Delete movie
+6) Search about movie
+7) Filter movies
 0) Exit program\n`);
     let step = input("Enter number to do! ");
 
@@ -169,173 +198,23 @@ const main = () => {
         printCatalog();
         break;
       case "4":
-        deleteMovie();
+        catalogArray = addAPIMoviesToCatalog(await getMovies());
+        writeJson(catalogArray);
         break;
       case "5":
-        searchAboutMovie();
+        deleteMovie();
         break;
       case "6":
+        searchAboutMovie();
+        break;
+      case "7":
         filterMovies();
         break;
       case "0":
-        writeJson(catalogArray);
+          writeJson(catalogArray);
         return;
     }
   }
 };
 
-// main();
-
-// import fs from "fs";
-// import prompt from "prompt-sync";
-// import Movie from "./movie.js";
-// const input = prompt();
-// let catalogArray = [];
-
-// const readJson = () => {
-//   try {
-//     const dataFromJson = JSON.parse(fs.readFileSync("./catalog.json", "utf-8"));
-//     return dataFromJson;
-//   } catch (err) {
-//     console.log("Error happened while reading");
-//     console.log(err.message);
-//   }
-// };
-
-// const writeJson = (data) => {
-//   try {
-//     const myData = JSON.stringify(data, null, 2);
-//     fs.writeFileSync("./test.json", myData, "utf-8");
-//   } catch (err) {
-//     console.log(err.message);
-//   }
-// };
-
-// catalogArray = readJson();
-
-// const addMovie = () => {
-//   let movieId = catalogArray[catalogArray.length - 1].id + 1;
-//   let title = input("Enter title: ");
-//   let releaseYear = input("Enter year: ");
-//   let genre = input("Enter genre: ");
-//   let description = input("Enter description: ");
-//   let rate = input("Enter rate: ");
-//   const movie = new Movie(
-//     movieId,
-//     title,
-//     releaseYear,
-//     genre,
-//     description,
-//     rate
-//   );
-//   JSON.parse(catalogArray).push(movie);
-//   writeJson(JSON.parse(catalogArray));
-// };
-
-// const updateMovie = () => {
-//   let movieId = input("Enter the movie ID to update: ");
-//   for (let i = 0; i < catalogArray.length; i++) {
-//     if (catalogArray[i].Id === movieId) {
-//       let newTitle = input("Enter the new title: ");
-//       let newReleaseYear = input("Enter the new release year: ");
-//       let newGenre = input("Enter the new genre: ");
-//       let newDescription = input("Enter the new description: ");
-//       let newRate = input("Enter the new rate: ");
-//       catalogArray[i] = {
-//         Id: movieId,
-//         Title: newTitle,
-//         Year: newReleaseYear,
-//         Genre: newGenre,
-//         Description: newDescription,
-//         Rate: newRate,
-//       };
-//       break;
-//     }
-//   }
-//   writeJson(JSON.parse(catalogArray, null, 2));
-// };
-
-// const printCatalog = () => {
-//   catalogArray.forEach((key) => console.log(key));
-// };
-
-// const deleteMovie = () => {
-//   let movieId = input("Enter the movie ID to delete: ");
-//   for (let i = 0; i < catalogArray.length; i++) {
-//     if (catalogArray[i].Id === movieId) {
-//       dataFromJson.splice(i, 1);
-//       break;
-//     }
-//   }
-//   writeJson(JSON.parse(catalogArray, null, 2));
-// };
-
-// const searchAboutMovie = () => {
-//   let movieTitle = input("Enter the movie title to search about: ");
-//   for (let i = 0; i < catalogArray.length; i++) {
-//     if (catalogArray[i].Title === movieTitle) {
-//       console.log(`Movie Id: ${catalogArray[i].Id}
-//                     Title: ${catalogArray[i].Title}
-//                     Released in: ${catalogArray[i].Year}
-//                     Directed by: ${catalogArray[i].Director}
-//                     Description: ${catalogArray[i].Description}`);
-//       break;
-//     }
-//   }
-//   writeJson(JSON.parse(catalogArray, null, 2));
-// };
-
-// const filterMovies = () => {
-//   let filterBy = input(
-//     "Enter the criteria to filter by (year, genre, or director): "
-//   );
-//   let filterValue = input("Enter the filter value: ");
-//   let filteredData = catalogArray.filter((movie) => {
-//     return movie[filterBy] === filterValue;
-//   });
-//   console.log(filteredData);
-//   writeJson(JSON.parse(catalogArray, null, 2));
-// };
-
-// const main = () => {
-//   let dataFromJson = readJson();
-//   writeJson(dataFromJson);
-//   while (true) {
-//     console.log(`===========================================
-// ----------- MOVIES CATALOG INFO -----------
-// ===========================================
-// 1) ADD new movie
-// 2) Update movie
-// 3) Display movies catalog
-// 4) Delete movie
-// 5) Search about movie
-// 6) Filter movies
-// 0) Exit program\n`);
-//     let step = input("Enter number to do! ");
-
-//     switch (step) {
-//       case "1":
-//         addMovie();
-//         break;
-//       case "2":
-//         updateMovie(dataFromJson);
-//         break;
-//       case "3":
-//         printCatalog(dataFromJson);
-//         break;
-//       case "4":
-//         deleteMovie(dataFromJson);
-//         break;
-//       case "5":
-//         searchAboutMovie(dataFromJson);
-//         break;
-//       case "6":
-//         filterMovies(dataFromJson);
-//         break;
-//       case "0":
-//         writeJson(JSON.parse(catalogArray, null, 2));
-//         return;
-//     }
-//   }
-// };
-// main();
+main();
